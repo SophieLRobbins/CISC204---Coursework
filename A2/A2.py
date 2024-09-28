20c217517c2ce6523534083a5c8e6011a11279f3
SophieLRobbins <144388361+SophieLRobbins@users.noreply.github.com>
2024-09-28 17:01:21 -0400

    almost final copy

--- A2/A2.py
+++ A2/A2.py
@@ -8,7 +8,7 @@ from lib204 import semantic_interface
 
 # Note: All of these answers are almost certainly wrong
 
-a2q12 = [ [s1], [s2], [s3], [s4] ]
+a2q12 = [ [s1], [s2, s3], [s4] ]
 
 a2q13 = {
     P: True,
@@ -20,26 +20,46 @@ a2q13 = {
 
 a2s5nnf = [
     [(~((Q&P)|R)>>(~Q|R)), 'starting formula'],
+    [(~(~((Q&P)|R))|(~Q|R)), 'material implication'],
+    [(((Q&P)|R)|(~Q|R)), 'double negation'],
 ]
 
 a2s6nnf = [
     [(~(R|Q)>>~(P|~R)), 'starting formula'],
+    [((~R&~Q)>>(~P&~~R)), 'de Morgans'], 
+    [((~R&~Q)>>(~P&R)), 'double negation'],
+    [((~(~R&~Q))|(~P&R)), 'material implication'],
+    [((~~R|~~Q)|(~P&R)), 'de Morgans'],
+    [((R|Q)|(~P&R)), 'double negation']
 ]
 
 a2s5cnf = [
     [(~((Q&P)|R)>>(~Q|R)), 'starting formula'],
+     [(~(~((Q&P)|R))|(~Q|R)), 'material implication'],
+    [(((Q&P)|R)|(~Q|R)), 'double negation'],
+    [((Q|R)&(P|R)&(~Q|R)), 'distribution']
 ]
+
 
 a2s6cnf = [
     [(~(R|Q)>>~(P|~R)), 'starting formula'],
+    [((~R&~Q)>>(~P&~~R)), 'de Morgans'], 
+    [((~R&~Q)>>(~P&R)), 'double negation'],
+    [((~(~R&~Q))|(~P&R)), 'material implication'],
+    [((~~R|~~Q)|(~P&R)), 'de Morgans'],
+    [((R|Q)|(~P&R)), 'double negation']
 ]
 
 a2s5tseitin = semantic_interface.Encoding()
-# Fill in the Tseitin steps and finalize
-x1 = a2s5tseitin.tseitin(P | Q, 'x1') # just an example
-a2s5tseitin.finalize(x1) # make sure you update the variable!
+x1 = a2s5tseitin.tseitin(Q & P, 'x1')
+x2 = a2s5tseitin.tseitin(x1 | R, 'x2')
+x3 = a2s5tseitin.tseitin(~x2, 'x3')
+x4 = a2s5tseitin.tseitin(~Q | R, 'x4')
+a2s5tseitin.finalize(x4) 
 
 a2s6tseitin = semantic_interface.Encoding()
-# Fill in the Tseitin steps and finalize
-x1 = a2s6tseitin.tseitin(P | Q, 'x1') # just an example
-a2s6tseitin.finalize(x1) # make sure you update the variable!
+x1 = a2s6tseitin.tseitin(R | Q, 'x1') 
+x2 = a2s6tseitin.tseitin(~x1, 'x2')
+x3 = a2s6tseitin.tseitin(P | ~R, 'x3')
+x4 = a2s6tseitin.tseitin(~x3, 'x4')
+a2s6tseitin.finalize(x4) 
